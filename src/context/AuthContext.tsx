e0bd3fa61c5122afc489f307f50d1f0c3f80d5a3
pallomagualter <pallomagualter@gmail.com>
2022-12-07 19:00:46 -0300

    feat: connecting frontend with backend

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,8 @@
 import { createContext, useCallback, useEffect, useState } from "react";
 
-interface ContextData {
+import { SignInData, SignUpData, singIn, singUp, me } from "../services/resources/user";
+
+interface UserDTO {
   id: string;
   firstName: string;
   lastName: string;
@@ -9,15 +11,46 @@ interface ContextData {
   accountDigit: number;
   wallet: string;
 }
+interface ContextData {
+  user: UserDTO;
+  userSignIn: (userData: SignInData) => void;
+  userSignUp: (userData: SignUpData) => void;
+}
 
 const AuthContext = createContext<ContextData>({} as ContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
 
-  const [user, setUser] = useState<ContextData>({} as ContextData);
+  const [user, setUser] = useState<UserDTO>({} as UserDTO);
+
+  const userSignIn = async (userData: SignInData) => {
+    const {data} = await singIn(userData);
+
+    if(data?.status === 'error'){
+      return data;
+    }
+
+
+    if (data.accessToken) {
+      localStorage.setItem('@GualterBank:Token', data.accessToken);
+    }
+
+    await getCurrentUser();
+  }
+
+  const getCurrentUser = async () => {
+    const {data} = await me();
+    setUser(data);
+    return data;
+  }
+
+  const userSignUp = async (userData: SignUpData) => {
+    const {data} = await singUp(userData);
+    localStorage.setItem('@GualterBank:Token', data.accessToken);
+  }
 
   return (
-    <AuthContext.Provider value={user}>
+    <AuthContext.Provider value={{user, userSignIn, userSignUp}}>
       {children}
     </AuthContext.Provider>
   )
