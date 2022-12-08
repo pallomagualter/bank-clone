7f7648eb9934946d82698fa24239bc5b0f53bfda
pallomagualter <pallomagualter@gmail.com>
2022-12-08 15:35:34 -0300

    feat: using interceptors

---
+++ src/pages/Billet/index.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export default function Billet() {
+  const [age, setAge] = useState('');
+
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,13 @@ const api = axios.create({
   baseURL: `http://localhost:5173`,
 })
 
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('@GualterBank') || '';
+  config.headers = {
+    'Authorization': `Bearer ${token}`,
+  }
+
+  return config;
+})
+
 export default api;
